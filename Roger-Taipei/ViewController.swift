5b82cc29a9549cfaa15d38af265ef216a73d4bf0
rogerthenext <rogerexcia@gmail.com>
2016-07-25 17:40:24 -0700

    Initial Commit

---
+++ Roger-Taipei/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Roger-Taipei
+//
+//  Created by RogerChen on 2016/7/25.
+//  Copyright © 2016年 Roger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
